a6ac294d8fb4ab2cfe9c62d4f044ddbed265c439
habimanasharif <hdidiersharif@gmail.com>
2024-03-04 08:39:32 +0200

    implimented about us

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import React from "react"
 import styled from "styled-components";
 import Hero from "../components/hero";
+import About from "../components/About";
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -9,6 +10,7 @@ const StyledMainContainer = styled.main`
 export default function Page() {
     return <StyledMainContainer className="fillHeight">
      <Hero/>
+     <About/>
     </StyledMainContainer>
       
   }
